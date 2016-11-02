7b8ccc0af4ab9799e5e3bc97d32ebdd6047bbf35
Daniel Schulte <daniel@schultes-home.de>
2016-11-02 23:55:26 +0100

    Add delay utility function

--- util.h
+++ util.h
@@ -1,4 +1,15 @@
 #pragma once
+#include <stdint.h>
 
 #define UNUSED(x) ((void)x);
 extern unsigned long systicks;
+
+inline void delay(uint32_t usecs)
+{
+    uint32_t ctr=(48000000ul*usecs)/3/1000000;
+    while(ctr)
+    {
+        __asm("");
+        --ctr;
+    }
+}
